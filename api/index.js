a353787149c20a3ed438c916e3fdd7393734e337
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-18 21:32:09 -0300

    chore: update shareCode to 5033880EJTK

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=IB4IK7G2J55&afp2=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=5033880EJTK&afp2=tapib' });
     res.end();
   }
